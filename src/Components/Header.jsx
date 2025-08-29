cf3bf1a1751205adb71452199bdb8bb5bb079b0a
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-08-29 23:05:31 +0200

    Modifica nel header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <div className="container ">
+    <div className="container-fluid">
       <div className="row">
         <div className="col-12 p-3 mb-4 bg-light">
           <h1>Learn Web development</h1>
